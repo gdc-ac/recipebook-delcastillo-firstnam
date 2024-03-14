f7c132324225f62dda2a687c52bd64ede74f78c0
michael-gdc <michael.delcastillo@student.ateneo.edu>
2024-03-14 21:46:48 +0800

    changed RecipeDetailView to require user to be logged in

--- ledger/views.py
+++ ledger/views.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from .models import Recipe
-from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from .models import Recipe
+
 
 # Create your views here.
 class RecipeListView(ListView):
     model = Recipe
-    template_name = 'list.html'
+    template_name = "list.html"
 
-class RecipeDetailView(DetailView):
+
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
-    template_name = 'recipe_detail.html'
+    template_name = "recipe_detail.html"
